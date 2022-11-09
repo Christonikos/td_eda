e845250c716bedc7a7fca2ecd5ac6198519198c4
Christos-Nikolaos Zacharopoulos <christonik@gmail.com>
2022-11-09 12:28:21 +0100

    added CSS styles

--- eda_report.py
+++ eda_report.py
@@ -13,7 +13,21 @@ from streamlit_pandas_profiling import st_profile_report
 import pandas_profiling
 import pandas as pd
 
-# define the root path where the raw data are stored
+
+st.set_page_config(page_title='TD-EDA Report',
+                   page_icon=":brain:", layout='wide')
+# =============================================================================
+# LOCAL CSS
+# =============================================================================
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
 
 tsv_fname = 'participants.tsv'
 tsv_file = pd.read_csv(tsv_fname,
