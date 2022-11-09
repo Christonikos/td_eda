6e9c325bb5d00f53903563e1839eb3e97b5c20bf
Christos-Nikolaos Zacharopoulos <christonik@gmail.com>
2022-11-09 11:07:50 +0100

    streamlit report implementation

---
+++ eda_report.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Generate TD-Brain Dataset report and deploy it via Streamlit
+@author: christos
+"""
+
+# =============================================================================
+# IMPORT MODULES
+# =============================================================================
+import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
+import os
+import pandas_profiling
+import pandas as pd
+
+# define the root path where the raw data are stored
+root_path = '/Volumes/storage/TD-BRAIN-DATASET/'
+tsv_fname = os.path.join(root_path, 'participants.tsv')
+tsv_file = pd.read_csv(tsv_fname,
+                       sep='\t',
+                       index_col=False,
+                       low_memory=False)
+# generate profile
+profile = pandas_profiling.ProfileReport(tsv_file, title='TD-Brain EDA')
+st.title("TD-Brain EDA")
+
+st.write(tsv_file)
+st_profile_report(profile)
